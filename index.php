b9c01269434a47c2e71741be613e9858342ecde6
swapnilg07 <sgmcanitt@gmail.com>
2020-05-19 02:25:35 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<title>Technofania19</title>
+<title>Technofania'19</title>
 	<meta charset="utf-8">
   	<meta name="viewport" content="width=device-width, initial-scale=1">
  	<link rel="stylesheet" href="css2/bootstrap.css">
